50686be048cfcd54dd733422729bb3842460376b
Mahkii900 <col.graff.battleschool@gmail.com>
2019-07-01 15:42:23 -0600

    black diamond commit

--- Part1/index.js
+++ Part1/index.js
@@ -5,40 +5,50 @@ function play(clickedId) {
     const playerSpan = document.getElementById('player')
     const clickedElement = document.getElementById(clickedId)
 
-    if (playerSpan.innerText === 'X') {
-        playerSpan.innerText = 'O'
-        clickedElement.innerText = 'X'
-        board[clickedId] = 'X'
-    } else {
-        playerSpan.innerText = 'X'
-        clickedElement.innerText = 'O'
-        board[clickedId] = 'O'
+    if (clickedElement.innerText !== 'X' && clickedElement.innerText !== 'O') {
+        if (playerSpan.innerText === 'X') {
+            playerSpan.innerText = 'O'
+            clickedElement.innerText = 'X'
+            board[clickedId] = 'X'
+        } else {
+            playerSpan.innerText = 'X'
+            clickedElement.innerText = 'O'
+            board[clickedId] = 'O'
+        }
     }
 
     if (board[1] === board[2] && board[0] === board[1] && board[0] !== undefined) {
         //Top Across
         alert(`${board[0]}'s wins!`)
+        reset(playerSpan)
     } else if (board[0] === board[4] && board[0] === board[8] && board[0] !== undefined) {
         //Left Diagonal
         alert(`${board[0]}'s wins!`)
+        reset(playerSpan)
     } else if (board[0] === board[3] && board[0] === board[6] && board[0] !== undefined) {
         //Left Down
         alert(`${board[0]}'s wins!`)
+        reset(playerSpan)
     } else if (board[1] === board[4] && board[1] === board[7] && board[1] !== undefined) {
         //Center Down
         alert(`${board[1]}'s wins!`)
+        reset(playerSpan)
     } else if (board[2] === board[5] && board[2] === board[8] && board[2] !== undefined) {
         //Right Down
         alert(`${board[2]}'s wins!`)
+        reset(playerSpan)
     } else if (board[2] === board[4] && board[2] === board[6] && board[2] !== undefined) {
         //Right Diagonal
         alert(`${board[2]}'s wins!`)
+        reset(playerSpan)
     } else if (board[3] === board[4] && board[3] === board[5] && board[3] !== undefined) {
         //Middle Across
         alert(`${board[3]}'s wins!`)
+        reset(playerSpan)
     } else if (board[6] === board[7] && board[6] === board[8] && board[6] !== undefined) {
         //Bottom Across
         alert(`${board[6]}'s wins!`)
+        reset(playerSpan)
     }
 
     let boardFull = true;
@@ -49,5 +59,15 @@ function play(clickedId) {
     }
     if (boardFull === true) {
         alert("Cat's game")
+        reset(playerSpan)
+    }
+}
+
+function reset(pSpan) {
+    board = []
+    pSpan.innerText = 'X'
+    for (let i = 0; i < 9; i++) {
+        let cEle = document.getElementById(i)
+        cEle.innerText = null
     }
 }
